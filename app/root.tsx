a8c032e9dc4a0578e5cdb7c197ecd0c3aed87087
Zach Burke <zach.burke@kaizen.io>
2023-12-05 18:29:20 -0600

    updating page on login/logout

--- app/root.tsx
+++ app/root.tsx
@@ -9,7 +9,7 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import netlifyIdentity from "netlify-identity-widget";
-import { createContext, useEffect } from "react";
+import { createContext, useEffect, useState } from "react";
 import styles from "~/styles/site.css";
 
 export const links: LinksFunction = () => [
@@ -17,12 +17,18 @@ export const links: LinksFunction = () => [
 ];
 
 export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   useEffect(() => {
     netlifyIdentity.init();
-
+    netlifyIdentity.on("login", () => {
+      setIsLoggedIn(true);
+    });
+    netlifyIdentity.on("logout", () => {
+      setIsLoggedIn(false);
+    });
+    const user = netlifyIdentity.currentUser();
+    setIsLoggedIn(user && user.id);
   });
-  const user = netlifyIdentity.currentUser();
-  const isAuthed = user && user.id;
   return (
     <html lang="en">
       <head>
@@ -41,12 +47,12 @@ export default function App() {
         </header>
         <main>
           <div>
-            {!isAuthed && <button onClick={() => netlifyIdentity.open()}>Login with Netlify</button>}
-            {isAuthed && <button onClick={() => netlifyIdentity.logout()}>Logout</button>}
+            {!isLoggedIn && <button onClick={() => netlifyIdentity.open()}>Login with Netlify</button>}
+            {isLoggedIn && <button onClick={() => netlifyIdentity.logout()}>Logout</button>}
           </div>
 
-          {isAuthed && <Outlet />}
-          {!isAuthed && <div>Not logged in</div>}
+          {isLoggedIn && <Outlet />}
+          {!isLoggedIn && <div>Not logged in</div>}
         </main>
         <ScrollRestoration />
         <Scripts />
