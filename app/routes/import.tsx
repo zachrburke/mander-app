d7d4372209e59a84f709cb5f50a806b384d19253
Zach Burke <zach.r.burke@gmail.com>
2024-05-16 21:28:24 -0500

    url encoding the cursor

--- app/routes/import.tsx
+++ app/routes/import.tsx
@@ -43,7 +43,7 @@ function applySyncResult(itemData: ItemData, syncResult: plaidApi.TransactionSyn
 async function getSyncResult(itemId: string, cursor: string | null): Promise<plaidApi.TransactionSyncResult> {
   let url = `/import/sync?itemId=${itemId}`;
   if (cursor) {
-    url += `&cursor=${cursor}`;
+    url += `&cursor=${urlencode(cursor)}`;
   }
   const response = await fetch(url);
   return await response.json();
@@ -80,4 +80,8 @@ export default function Import() {
       <h3>Syncing {data.items.length} item(s)</h3>
     </div>
   );
+}
+
+function urlencode(cursor: string) {
+  throw new Error('Function not implemented.');
 }
